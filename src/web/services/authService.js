6cc1c9459f5c881ca14e6e09db3be85c38c7ff5a
PhonsekalLaure <rosaritoronnsaimund@gmail.com>
2026-04-21 14:15:10 +0800

    chore: apply path alias

--- src/web/services/authService.js
+++ src/web/services/authService.js
@@ -1,4 +1,4 @@
-const { supabase, supabaseAdmin } = require('../../supabaseClient');
+const { supabase, supabaseAdmin } = require('@src/supabaseClient');
 
 /**
  * Roles that are allowed to log in through the web portal.
