858d7c22f861aa3ba326e8d768419ef997c74bc9
Belle Acedillo <belleacedillo@gmail.com>
2026-04-25 23:09:45 +0800

    refactor(api): add shift property to filterMiddleware

--- src/web/middlewares/filterMiddleware.js
+++ src/web/middlewares/filterMiddleware.js
@@ -2,13 +2,14 @@
  * Middleware to parse filtering query parameters
  */
 function filterMiddleware(req, res, next) {
-  const { search, status, client } = req.query;
+  const { search, status, client, shift } = req.query;
 
   // Standardize the filters object
   req.filters = {
     search: search || null,
     status: status || 'all',
-    client: client || 'all'
+    client: client || 'all',
+    shift: shift || 'all',
   };
 
   next();
